31de96aa7a4719f36b72e64688332193a7d43c88
Efrat Kreslavskiy <ekreslav@student.touro.edu>
2026-02-18 17:43:05 -0500

    changed origin to bottom left corner

--- src/main/java/kreslavskiy/physics/GravityComponent.java
+++ src/main/java/kreslavskiy/physics/GravityComponent.java
@@ -3,16 +3,15 @@ package kreslavskiy.physics;
 import javax.swing.*;
 import java.awt.*;
 
-public class GravityComponent extends JComponent
-{
+public class GravityComponent extends JComponent {
     @Override
-    protected void paintComponent(Graphics g)
-    {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.translate(0, getHeight());
 
         //g.drawLine(0, 0, getWidth(), getHeight());
         //g.drawOval(30, 45, 20, 20);                    //draws outline of oval
-        //g.fillOval(0, 0, 20, 20); //draws filled oval
+        //g.fillOval(0, 0, 20, 20);                      //draws filled oval
 
         Force gravity = new Force(0, -9.8);
         Force scaledGravity = gravity.scale(0.001);
@@ -21,14 +20,13 @@ public class GravityComponent extends JComponent
         double y = 0.0;
         Force f1 = new Force(37.0365, 28.9360);
 
-        for (double i = 0; i < 5; i += 0.001)
-        {
+        for (double i = 0; i < 5; i += 0.001) {
             f1 = f1.addForce(scaledGravity);
             Force scaledF1 = f1.scale(0.001);
 
             x += scaledF1.getX();
             y += scaledF1.getY();
-            g.fillOval((int) (x), (int) (getHeight() - y), 20, 20);
+            g.fillOval((int) (x), (int) (-y), 5, 5);
 
         }
     }
